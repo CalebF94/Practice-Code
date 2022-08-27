d1f1c832753d416c9cabfb29fc0ddb1d7416128f
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2022-08-26 21:36:36 -0500

    Add files via upload

---
+++ Python/RealPython/Learning Path: Intro to Python/python-modules-and-packages/pkg/mod3.py
@@ -0,0 +1,6 @@
+def send_mail():
+    print('Sending_mail function run')
+
+
+class Winner():
+    pass
---
+++ Python/RealPython/Learning Path: Intro to Python/python-modules-and-packages/pkg/mod4.py
@@ -0,0 +1,6 @@
+def merge_date():
+    print("merging data with merge_data()")
+
+
+class Message():
+    pass
