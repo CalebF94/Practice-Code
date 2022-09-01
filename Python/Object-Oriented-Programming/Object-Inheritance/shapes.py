c5f81c036e2bc6802defb32303c964d67589ef37
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2022-08-31 21:54:57 -0500

    Add files via upload

--- Python/Object-Oriented-Programming/Object-Inheritance/shapes.py
+++ Python/Object-Oriented-Programming/Object-Inheritance/shapes.py
@@ -1,5 +1,67 @@
 # simple class
-class Square:
-    pass
+# class Square:
+#     # This is the class constructor. There's 1 parameter length that needs to be
+#     # be supplied when instantiating a square
+#     def __init__(self, length):
+#         self.length = length
+#
+#     # Below are methods for the Square Class
+#     def area(self):
+#         return self.length * self.length
+#
+#     def perimeter(self):
+#         return 4 * self.length
 
 
+# We also want to create a rectangle
+class Rectangle:
+    # a rectangle has two attributes
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    # Again create perimeter and area methods
+    def what_am_i(self):
+        return "Rectangle"
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+# It's better for the Square class to inharate from the Rectangle class
+
+# notation for inheriting from Rectangle. If we don't change anything the Square
+# class will be the same as the Rectangle class
+class Square(Rectangle):
+    # here we are overwriting the constructor
+    def __init__(self, length):
+            super().__init__(length, length)
+            #super allows us to access the parents methods
+            # in this case we're changing the __init__function
+            # that's it Python will automatically change all references of width
+            # to length
+
+
+    def what_am_i(self):
+        return "Square"
+
+
+class Cube(Square):
+    # same parameters as square so we don't need to redefine __init__. Cubes
+    # will be instantiated with only one parameter, like a square, but
+    # cube has two methods that aren't in its parent
+
+    def what_am_i(self):
+        return "Cube"
+
+    # adding methods that only belong to Cube
+    def surface_area(self):
+        face_area = self.area()
+        return face_area * 6
+
+    def volume(self):
+        face_area = super().area()
+        return face_area * self.length
