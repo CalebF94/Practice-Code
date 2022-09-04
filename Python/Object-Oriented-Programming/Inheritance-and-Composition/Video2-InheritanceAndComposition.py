b8dc1f7ac8fd38e17cfc999d9f8aa3dfc06e36bb
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2022-09-03 20:53:38 -0500

    Add files via upload

---
+++ Python/Object-Oriented-Programming/Inheritance-and-Composition/Video2-InheritanceAndComposition.py
@@ -0,0 +1,14 @@
+"""
+*INHERITANCE AND COMPOSITION
+
+These are two different techniques to write code that modify classes to write code that is
+more extensible (easily changed without rewriting entire code base).
+
+Inheritance:
+    - Identifying the common attributes and behaviors
+    - some classes will share attributes and methods or with slight modifictions
+
+Composition:
+    - How are objects in the real world composed of one another
+    - Attribute types can move beyond the primitive to types that we define ourselves
+"""
