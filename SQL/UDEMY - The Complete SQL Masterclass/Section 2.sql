d8e1d7976cece12b8a679f715a9b5a1a52eaddc7
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2023-01-02 19:33:02 -0600

    Add files via upload

--- SQL/UDEMY - The Complete SQL Masterclass/Section 2.sql
+++ SQL/UDEMY - The Complete SQL Masterclass/Section 2.sql
@@ -2,6 +2,7 @@
 -- Udemy Course Challenge/Practice Problems
 -- Section 2: Filtering
 
+-------------------------------------------------------------------------------
 -- Challenge: WHERE
 -- Notes: Clause directly follows the FROM clause
 -- How many payments were made by the customer with customer_id = 100 (24)
@@ -13,4 +14,79 @@ where customer_id = 100;
 -- MATTHEWS
 SELECT first_name, last_name
 FROM customer
-WHERE first_name = 'ERICA'
+WHERE first_name = 'ERICA';
+
+
+-------------------------------------------------------------------------------
+-- Challenge: WHERE Operators
+-- Notes: Options include =, >, <, <=, >=, !=, is null, is not null
+-- How many rentals have not been returned yet (return_date is null)(183)
+SELECT COUNT(*) 
+FROM rental
+WHERE return_date is null;
+
+-- List of all payment_ids with an amount less than or equal to $2. (3644 rows)
+SELECT payment_id, amount
+FROM payment
+WHERE amount <= 2.00;
+
+-------------------------------------------------------------------------------
+-- Challenge: Where with AND/OR
+-- Notes:
+-- list of payments for customer 322, 346, and 354 where the amount is either
+--   less than $2 or greater than $10
+--   Order by customer first then amount descending
+
+SELECT *
+FROM payment
+WHERE customer_id IN (322, 346, 354)
+	AND (amount < 2 OR amount > 10)
+ORDER BY 
+	customer_id, amount DESC;
+
+-------------------------------------------------------------------------------
+-- Challenge: BETWEEN
+-- Notes: When using dates in the format "yyyy-mm-dd" the timestamp is assigned
+-- to midnight (00:00).
+
+-- How many payments have been made on Jan 26th and 27th 2020 with an amount
+-- between 1.99 and 3.99 (101)
+SELECT COUNT(*)
+FROM payment
+WHERE (payment_date BETWEEN '2020-01-26 00:00' AND '2020-01-27 23:59')
+	AND (amount BETWEEN 1.99 AND 3.99);
+
+
+-------------------------------------------------------------------------------
+-- Challenge: IN
+-- Notes: The IN clause can be used to replace multiple OR conditions
+
+-- Find payments for customers 12, 25, 67, 93, 124, and 234 that occured in
+-- January 2022 with the amounts of 4.99, 7.99, and 9.99
+SELECT customer_id, payment_id
+FROM payment
+WHERE (customer_id IN (12, 25, 67, 93, 124, 234))
+	   AND (payment_date BETWEEN '2020-01-01' AND '2020-01-31 23:59')
+	   AND (amount IN (4.99, 7.99, 9.99));
+
+
+-- Challenge: LIKE
+-- Notes: '_' matches any single character. '%' matches any number of chars
+--        Like is case sensitive. ILIKE is case insensitive
+
+-- How many movies are there that contain 'Documentary' in the description (101)
+SELECT COUNT(*)
+FROM film
+WHERE description LIKE '%Documentary%';
+
+-- How many customers have first_name that is 3 letters long with an 'X' or 'Y' as
+-- the last letter? (3)
+SELECT COUNT(*)
+FROM customer
+WHERE (first_name LIKE '___')
+	AND (last_name LIKE '%Y' OR last_name LIKE '%X');
+
+
+
+
+
