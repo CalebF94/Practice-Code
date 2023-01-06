268fd2a23ad0604b7608bb989f2e203d7f5a2cef
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2023-01-05 21:51:59 -0600

    Add files via upload

--- SQL/UDEMY - The Complete SQL Masterclass/Section 4.sql
+++ SQL/UDEMY - The Complete SQL Masterclass/Section 4.sql
@@ -71,3 +71,92 @@ SELECT
 FROM
 	customer
 
+
+/*
+CHALLENGE: EXTRACT
+*/
+-- What's the month with highest total payment amount? (april)
+SELECT
+	EXTRACT(month from payment_date) as month,
+	SUM(amount) as Amount
+FROM 
+	payment
+GROUP BY month
+ORDER BY Amount DESC;
+
+-- What day of week has highest total payment (wednesday)
+SELECT
+	EXTRACT(dow from payment_date) as day_of_week,
+	SUM(amount) as Amount
+FROM 
+	payment
+GROUP BY day_of_week
+ORDER BY Amount DESC;
+
+
+-- What's the highest amount one customer has spent in a week (73.88)
+SELECT
+	customer_id,
+	EXTRACT(week from payment_date) as week,
+	SUM(amount) as Amount
+FROM 
+	payment
+GROUP BY customer_id, week
+ORDER BY Amount DESC
+LIMIT 1;
+
+
+/*
+CHALLENGE: TO CHAR
+*/
+--Aggregate payments and group by day format day, dd/mm/yyyy
+SELECT 
+	TO_CHAR(payment_date, 'Dy, dd/mm/yyyy') as "Date",
+	SUM(amount) as Amount
+FROM payment
+GROUP BY "Date";
+
+
+--Aggregate payments and group by day format month, yyyy
+SELECT 
+	TO_CHAR(payment_date, 'Mon, yyyy') as "Month",
+	SUM(amount) as Amount
+FROM payment
+GROUP BY "Month";
+
+--Aggregate payments and group by day format dy, hh:mm
+SELECT 
+	TO_CHAR(payment_date, 'Dy, hh:mi') as "Date",
+	SUM(amount) as Amount
+FROM payment
+GROUP BY "Date";
+
+
+/*
+CHALLENGE: INTERVALS and TIMESTAMPS
+*/
+--Create a list for all rental durations of customer with id 35
+SELECT 
+	customer_id,
+	return_date-rental_date as rental_length
+FROM
+	rental
+WHERE 
+	customer_id = 35;
+	
+	
+-- Find which customer has the longest average rental duration
+SELECT
+	customer_id,
+	AVG(return_date-rental_date) as rental_length,
+	COUNT(*) as rentals
+FROM rental
+GROUP BY customer_id
+ORDER BY rental_length;
+
+
+
+
+
+
+
