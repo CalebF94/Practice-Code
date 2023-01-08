02f0d63140e6f72a1d3a05f9ca3348e3f26af946
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2023-01-08 12:30:17 -0600

    Add files via upload

--- SQL/UDEMY - The Complete SQL Masterclass/Section 6 demo database.sql
+++ SQL/UDEMY - The Complete SQL Masterclass/Section 6 demo database.sql
@@ -31,5 +31,54 @@ WHERE b.boarding_no IS null;
 	
 	
 	
+/*
+CHALLENGE: LEFT JOIN
+
+-- FIND aircrafts that have not been used in any flights
+-- Finding aircraft where the flight id is null
+*/
+SELECT * 
+FROM aircrafts_data as a LEFT JOIN flights as f	
+	ON a.aircraft_code = f.aircraft_code
+WHERE a.aircraft_code is not null AND flight_id is null;
+	
+-- Which seat is the most commonly chosen spot?
+-- Make sure all seats are included even if they have never been chosen
+SELECT 
+	s.seat_no as seat,
+	COUNT(*) as times_chosen
+FROM seats AS s LEFT JOIN boarding_passes AS bp
+	ON s.seat_no = bp.seat_no
+GROUP BY seat
+ORDER BY times_chosen DESC;
+
+
+-- Which line(A, B,...H) has been chosen most
+SELECT 
+	RIGHT(s.seat_no, 1) as row,
+	COUNT(*) as times_chosen
+FROM seats AS s LEFT JOIN boarding_passes AS bs
+	ON s.seat_no = bs.seat_no
+GROUP BY row
+ORDER BY times_chosen DESC;
+	
+
+/*
+CHALLENGE: JOINS ON MULTIPLE CONDITIONS
+
+-- Find the average price for different seat numbers
+*/
+
+SELECT 
+	bp.seat_no as seat_no,
+	ROUND(AVG(tf.amount), 2) as avg_amount
+FROM ticket_flights AS tf LEFT JOIN boarding_passes AS bp 
+	ON tf.ticket_no = bp.ticket_no 
+	AND tf.flight_id = bp.flight_id
+GROUP BY seat_no
+ORDER BY avg_amount DESC;
+	
+	
+	
 	
 	
