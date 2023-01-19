2ea78d54f9b9eff4143939df1d9e0270b94ffb69
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2023-01-18 22:04:55 -0600

    Add files via upload

--- SQL/UDEMY - The Complete SQL Masterclass/Section 9 GreenCycle Challenges.sql
+++ SQL/UDEMY - The Complete SQL Masterclass/Section 9 GreenCycle Challenges.sql
@@ -23,3 +23,50 @@ film_id INT REFERENCES film(film_id),
 amount NUMERIC(5, 2) NOT NULL CHECK (amount BETWEEN 0.00 AND 999.99),
 promotion_code VARCHAR(10) DEFAULT('None')
 )
+
+
+/*
+INSERT Assignment
+
+Insert these values in the table online_sales:
+	- transaction_id (1,2,3)
+	- customer_id (124, 225, 119)
+	- film_id (65, 231, 53)
+	- amount (14.99, 12.99, 15.99)
+	- promotion_code (PROMO2022, JULYPROMO, SUMMERDEAL)
+*/
+INSERT INTO online_sales
+	(transaction_id, customer_id, film_id, amount, promotion_code)
+	VALUES (1, 124, 65, 14.99, 'PROMO2022'),
+		   (2, 225, 231, 12.99, 'JULYPROMO'),
+		   (3, 119, 53, 15.99, 'SUMMERDEAL');
+
+
+/*
+CHALLENGE: ALTER TABLE
+
+Make the following modifications to the director table:
+	- director_account_name to VARCHAR(30)
+	- drop the default on last_name
+	- add the constraint not null to last name
+	- add the column email of data type VARCHAR(40)
+	- rename the director_account_name to account_name
+	- rename the table from director to directors
+*/
+ALTER TABLE director
+	ALTER COLUMN director_account_name TYPE VARCHAR(30),-- type only needed for alter
+	ALTER COLUMN last_name DROP DEFAULT,
+	ALTER COLUMN last_name SET NOT NULL,
+	ADD COLUMN email VARCHAR(40);
+	
+ALTER TABLE director
+	RENAME director_account_name TO account_name;
+	
+ALTER TABLE director
+	RENAME TO directors;
+
+SELECT * FROM directors
+
+
+
+
