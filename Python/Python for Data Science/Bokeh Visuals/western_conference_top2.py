71b50cc5593dca590e6a237c857ccf6c15d9cad1
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2024-03-02 08:45:54 -0600

    Add files via upload

--- Python/Python for Data Science/Bokeh Visuals/western_conference_top2.py
+++ Python/Python for Data Science/Bokeh Visuals/western_conference_top2.py
@@ -1,6 +1,6 @@
 from bokeh.io import output_file
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, CDSView, GroupFilter
 
 # Import data
 from read_nba_data import *
@@ -8,31 +8,48 @@ from read_nba_data import *
 output_file('west_top_2_standings_race.html',
             title = 'Western Confernce Top 2 Teams Wins Race')
 
+## This was the process used in video 3
 # isolate data for each team
-rockets_data = west_top_2[west_top_2[:, 'teamAbbr']=='HOU']
-warriors_data = west_top_2[west_top_2[:, 'teamAbbr']=='GS']
+# rockets_data = west_top_2[west_top_2[:, 'teamAbbr']=='HOU']
+# warriors_data = west_top_2[west_top_2[:, 'teamAbbr']=='GS']
 
-# Create a column data source
-rockets_cds = ColumnDataSource(rockets_data)
-warriors_cds = ColumnDataSource(warriors_data)
+#  #Video 3: Created two column data sources and
+# rockets_cds = ColumnDataSource(rockets_data)
+# warriors_cds = ColumnDataSource(warriors_data)
 
-fig = figure(x_axis_type='datetime',
+## This is the process using Groupfilter and CDS view from video 4
+west_cds=ColumnDataSource(west_top_2)
+
+# create view for each team
+rockets_view = CDSView(filter=GroupFilter(column_name='teamAbbr', group='HOU'))
+warriors_view = CDSView(filter=GroupFilter(column_name='teamAbbr', group='GS'))
+
+west_fig = figure(x_axis_type='datetime',
              height=300, width=600,
              title='Western Conference Top 2 Teams Wins Race 2017-18',
              x_axis_label='Date', y_axis_label='Wins',
              toolbar_location=None
              )
 
-# Render the race as step lines
-fig.step(x='stDate', y='gameWon',
-         color = '#CE1141', legend='Rockets',
-         source = rockets_cds)
+# Render the race as step lines from video 3
+# west_fig.step(x='stDate', y='gameWon',
+#          color = '#CE1141', legend='Rockets',
+#          source = rockets_cds)
 
-fig.step(x='stDate', y='gameWon',
-         color = '#006BB6', legend='Warriors',
-         source = warriors_cds)
+# west_fig.step(x='stDate', y='gameWon',
+#          color = '#006BB6', legend='Warriors',
+#          source = warriors_cds)
+
+# From Video 4
+west_fig.step(x='stDate', y='gameWon',
+         color = '#CE1141', legend_label='Rockets',
+         source = west_cds, view=rockets_view)
+
+west_fig.step(x='stDate', y='gameWon',
+         color = '#006BB6', legend_label='Warriors',
+         source = west_cds, view=warriors_view)
 
 # move legend
-fig.legend.location = 'top_left'
+west_fig.legend.location = 'top_left'
 
-show(fig)
+show(west_fig)
